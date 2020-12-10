fd8d92befe947096491fab251bc81e3b0dab0332
nikos chalikias <nikos.chalikias@gmail.com>
2020-12-10 15:44:13 +0000

    GPI replaced with GPIN and Tested

---
+++ studio/export/12D_MotorTest/nlib/BSP/bsp.h
@@ -0,0 +1,70 @@
+/**
+ *  N-BLOCKS STUDIO BOARD SUPPORT PACKAGE
+ *  
+ *  This BSP supports the mbed LPC1768 board
+ */
+
+#ifndef __BSP_MBED
+#define __BSP_MBED
+
+#define IO1			p5
+#define IO2			p6
+#define IO3			p7
+#define IO4			p8
+#define IO5			p9
+#define IO6			p10
+#define IO7			p11
+#define IO8			p12
+#define IO9			p13
+#define IO10		p14
+#define IO11		p15
+#define IO12		p16
+#define IO13		p17
+#define IO14		p18
+#define IO15		p19
+#define IO16		p20
+#define IO17		p21
+#define IO18		p22
+#define IO19		p23
+#define IO20		p24
+#define IO21		p25
+#define IO22		p26
+#define IO23		p27
+#define IO24		p28
+#define IO25		p29
+#define IO26		p30
+
+#define PWM1		p21
+#define PWM2		p22
+#define PWM3		p23
+#define PWM4		p24
+#define PWM5		p25
+#define PWM6		p26
+
+#define ADC1		p15
+#define ADC2		p16
+#define ADC3		p17
+#define ADC4		p18
+#define ADC5		p19
+#define ADC6		p20
+
+#define TX1			p13
+#define RX1			p14
+#define TX2			p28
+#define RX2			p27
+#define TX3			p9
+#define RX3			p10
+
+#define MOSI1		p11
+#define MISO1		p12
+#define SCK1		p13
+#define MOSI2		p5
+#define MISO2		p6
+#define SCK2		p7
+
+#define SDA1		p9
+#define SCL1		p10
+#define SDA2		p28
+#define SCL2		p27
+
+#endif
